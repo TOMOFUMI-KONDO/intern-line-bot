9df555dbcd598439532bc94f97b3754da8321bb4
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-25 15:14:41 +0900

    When receive '一覧' message, response all lendings list with count per lender, content

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -37,8 +37,32 @@ class WebhookController < ApplicationController
                 lending_count = Lending.where(borrower_id: borrower_id, lender_name: lender_name).count
                 "#{lender_name}さんに#{content}を借りました！\n#{lender_name}さんには計#{lending_count}個の借りがあります。"
 
-              elsif action == "一覧"
-                '佐藤くん(2個)　100円　マスタリングTCP/IP借りた、田中くん(1個)　研究の調査を手伝ってもらった、......'
+              elsif messages[0] == "一覧" && messages.length == 1
+                lendings_per_lender = Lending
+                                        .where("borrower_id = ?", user_id)
+                                        .group(:lender_name, :content)
+                                        .select("lender_name, content, count(*)")
+                                        .each_with_object({}) do |lending, hash|
+                  lender = lending.lender_name.intern
+                  content = lending.content.intern
+                  count = lending.count
+
+                  if hash.key?(lender)
+                    hash[lender][content] = count
+                  else
+                    hash[lender] = { content => count }
+                  end
+                end
+
+                lendings_per_lender.map do |lender, content_counts|
+                  count_total = content_counts.inject(0) do |result, content_count|
+                    result + content_count[1]
+                  end
+                  count_per_content = content_counts.map do |content, count|
+                    "- #{content}　#{count}個"
+                  end
+                  ["#{lender}　#{count_total}個", *count_per_content].join("\n")
+                end.join("\n\n")
 
               elsif action == "返した"
                 "#{lender_name}さんに#{content}を返しました！"
