dd65d2a457dd53680949c3b15f2c7ebd6ec2766a
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-25 14:05:36 +0900

    Changed from 'assert invalid?' to 'assert_not valid?'

--- test/models/lending_test.rb
+++ test/models/lending_test.rb
@@ -8,16 +8,16 @@ class LendingTest < ActiveSupport::TestCase
 
   test 'borrower_id_must_be_presence' do
     lending = Lending.new(lender_name: 'Bob', content: 'book')
-    assert lending.invalid?
+    assert_not lending.valid?
   end
 
   test 'lender_name_must_be_presence' do
     lending = Lending.new(borrower_id: 'abcedfg', content: 'book')
-    assert lending.invalid?
+    assert_not lending.valid?
   end
 
   test 'content_id_must_be_presence' do
     lending = Lending.new(borrower_id: 'Bob', lender_name: 'book')
-    assert lending.invalid?
+    assert_not lending.valid?
   end
 end
