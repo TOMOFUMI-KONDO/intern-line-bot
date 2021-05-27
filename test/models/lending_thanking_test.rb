0e098ca23adb6c374735df0cad7ad3d31c70e94d
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-27 19:24:48 +0900

    Added abnormal validation test for LendingThanking model whose id is not associated

--- test/models/lending_thanking_test.rb
+++ test/models/lending_thanking_test.rb
@@ -15,4 +15,32 @@ class LendingThankingTest < ActiveSupport::TestCase
     lending_thanking = LendingThanking.new(lending_id: lendings(:one).id)
     assert_not lending_thanking.valid?
   end
+
+  test 'lending_id must be associated exist lending' do
+    lending_ids = lendings.map(&:id)
+    not_exist_lending_id = generate_not_repeatable_id(lending_ids)
+
+    thanking = LendingThanking.new(lending_id: not_exist_lending_id, thanking_id: thankings(:one).id)
+    assert_not thanking.valid?
+  end
+
+  test 'thanking_id must be associated exist thanking' do
+    thanking_ids = thankings.map(&:id)
+    not_exist_thanking_id = generate_not_repeatable_id(thanking_ids)
+
+    lending_thanking = LendingThanking.new(lending_id: lendings(:one).id, thanking_id: not_exist_thanking_id)
+    assert_not lending_thanking.valid?
+  end
+
+  private
+
+  # fixtureのデータと被らないidを生成するためのメソッド
+  def generate_not_repeatable_id(exist_ids)
+    not_repeatable_id = nil
+    loop do
+      not_repeatable_id = rand(1..exist_ids.length)
+      break unless exist_ids.include?(not_repeatable_id)
+    end
+    not_repeatable_id
+  end
 end
