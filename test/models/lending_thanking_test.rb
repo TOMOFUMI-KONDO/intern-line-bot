7b4eff7eeaeaafc5b122bffeb2bc704f939633d2
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-27 18:51:22 +0900

    Added abnormal validation test for LendingThanking model

--- test/models/lending_thanking_test.rb
+++ test/models/lending_thanking_test.rb
@@ -5,4 +5,14 @@ class LendingThankingTest < ActiveSupport::TestCase
     lending_thanking = LendingThanking.new(lending_id: lendings(:one).id, thanking_id: thankings(:one).id)
     assert lending_thanking.valid?
   end
+
+  test 'lending_id must be presence' do
+    lending_thanking = LendingThanking.new(thanking_id: thankings(:one).id)
+    assert_not lending_thanking.valid?
+  end
+
+  test 'thanking_id must be presence' do
+    lending_thanking = LendingThanking.new(lending_id: lendings(:one).id)
+    assert_not lending_thanking.valid?
+  end
 end
