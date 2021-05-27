a1900e884e19356eddd3d9b6849ffb72aeb8a80a
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-27 18:48:37 +0900

    Fixed Thanking model test along with adding LendingThanking model

--- test/models/thanking_test.rb
+++ test/models/thanking_test.rb
@@ -2,22 +2,17 @@ require 'test_helper'
 
 class ThankingTest < ActiveSupport::TestCase
   test 'valid Thanking model' do
-    thanking = Thanking.new(name: 'ice', url: 'https://example.com', lending_id: lendings(:one).id)
+    thanking = Thanking.new(name: 'ice', url: 'https://example.com')
     assert thanking.valid?
   end
 
   test 'name must be presence' do
-    thanking = Thanking.new(url: 'https://example.com', lending_id: lendings(:one).id)
+    thanking = Thanking.new(url: 'https://example.com')
     assert_not thanking.valid?
   end
 
   test 'url must be presence' do
-    thanking = Thanking.new(name: 'ice', lending_id: lendings(:one).id)
-    assert_not thanking.valid?
-  end
-
-  test 'lending_id must be presence' do
-    thanking = Thanking.new(name: 'ice', url: 'https://example.com')
+    thanking = Thanking.new(name: 'ice')
     assert_not thanking.valid?
   end
 end
