5505f6a35520e31284d8eb4cccedb8ca4c237d6e
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-27 11:36:56 +0900

    Added Thanking model abnormal test
    model whose presence value is nil is invalid.

--- test/models/thanking_test.rb
+++ test/models/thanking_test.rb
@@ -5,4 +5,19 @@ class ThankingTest < ActiveSupport::TestCase
     thanking = Thanking.new(name: 'ice', url: 'https://example.com', lending_id: lendings(:one).id)
     assert thanking.valid?
   end
+
+  test 'name must be presence' do
+    thanking = Thanking.new(url: 'https://example.com', lending_id: lendings(:one).id)
+    assert_not thanking.valid?
+  end
+
+  test 'url must be presence' do
+    thanking = Thanking.new(name: 'ice', lending_id: lendings(:one).id)
+    assert_not thanking.valid?
+  end
+
+  test 'lending_id must be presence' do
+    thanking = Thanking.new(name: 'ice', url: 'https://example.com')
+    assert_not thanking.valid?
+  end
 end
