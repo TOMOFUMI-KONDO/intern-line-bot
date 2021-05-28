b34d5b4b36cde6e4cb4d5d1075d8913c9e315cc5
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-28 11:03:38 +0900

    Created random_choice scope on Thanking

--- app/models/thanking.rb
+++ app/models/thanking.rb
@@ -3,4 +3,8 @@ class Thanking < ApplicationRecord
   has_many :lendings, through: :lending_thankings
 
   validates :name, :url, presence: true
+
+  scope :random_choice, -> {
+    find(Thanking.pluck(:id).sample)
+  }
 end
