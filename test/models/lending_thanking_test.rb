88dcf7cd31b05df077ffd06d628599b9bb22295e
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-05-28 13:28:33 +0900

    Simplified LendingThanking validation test

--- test/models/lending_thanking_test.rb
+++ test/models/lending_thanking_test.rb
@@ -17,30 +17,14 @@ class LendingThankingTest < ActiveSupport::TestCase
   end
 
   test 'lending_id must be associated exist lending' do
-    lending_ids = lendings.map(&:id)
-    not_exist_lending_id = generate_not_repeatable_id(lending_ids)
-
+    not_exist_lending_id = lendings.map(&:id).max + 1
     thanking = LendingThanking.new(lending_id: not_exist_lending_id, thanking_id: thankings(:one).id)
     assert_not thanking.valid?
   end
 
   test 'thanking_id must be associated exist thanking' do
-    thanking_ids = thankings.map(&:id)
-    not_exist_thanking_id = generate_not_repeatable_id(thanking_ids)
-
+    not_exist_thanking_id = thankings.map(&:id).max + 1
     lending_thanking = LendingThanking.new(lending_id: lendings(:one).id, thanking_id: not_exist_thanking_id)
     assert_not lending_thanking.valid?
-  end
-
-  private
-
-  # fixtureのデータと被らないidを生成するためのメソッド
-  def generate_not_repeatable_id(exist_ids)
-    not_repeatable_id = nil
-    loop do
-      not_repeatable_id = rand(1..exist_ids.length)
-      break unless exist_ids.include?(not_repeatable_id)
-    end
-    not_repeatable_id
   end
 end
